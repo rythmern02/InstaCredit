53ffa37fafe767aaf6a73f3a4d70fcb1fa9af260
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2023-12-09 19:56:38 +0530

    creation of product listing page

--- Flutter_InstaCredit/lib/screens/product_list_screen.dart
+++ Flutter_InstaCredit/lib/screens/product_list_screen.dart
@@ -1,1 +1,56 @@
+import 'package:bnpl_flutter/constants.dart';
+import 'package:bnpl_flutter/screens/product_detail_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+class ProductListScreen extends StatefulWidget {
+  const ProductListScreen({super.key});
+
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SvgPicture.asset(
+            'assets/images/background.svg',
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 28),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    style: IconButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                    icon: Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(1000),
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                      ),
+                    ),
+                  ),
+                      )
+        ],
+      ),
+    );
+  }
+}
