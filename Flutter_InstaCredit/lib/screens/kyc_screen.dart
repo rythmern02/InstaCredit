7d42f4e31c9a7afb5afeed7668094dac8a8b4cad
Swarna nagrani <swarnasn29@gmail.com>
2023-12-09 21:13:35 +0530

    finished the KYC screen

--- Flutter_InstaCredit/lib/screens/kyc_screen.dart
+++ Flutter_InstaCredit/lib/screens/kyc_screen.dart
@@ -37,4 +37,153 @@ class _KycScreenState extends ConsumerState<KycScreen> {
       });
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = ref.watch(authStateProvider);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        resizeToAvoidBottomInset: false,
+        body: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 36, left: 16),
+                child: Row(
+                  children: [
+                    Text(
+                      "KYC",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              PageviewIndicator(page: _currentPage, total: 3),
+              Expanded(
+                child: PageView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: _controller,
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    PersonalInfoView(),
+                    ContactInfo(),
+                    DocumentUpload()
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (_currentPage < 2) {
+                      if (_currentPage == 0) {
+                        if (auth.firstName == "" ||
+                            auth.lastName == "" ||
+                            auth.fathersName == "" ||
+                            auth.gender == "") {
+                          Fluttertoast.showToast(
+                            msg: "Please fill the above details fully.",
+                          );
+                          return;
+                        }
+
+                        if (DateTime.now().difference(auth.dob) <
+                            const Duration(hours: 24, days: 0)) {
+                          Fluttertoast.showToast(
+                            msg: "Please fill correct DOB",
+                          );
+                          return;
+                        }
+                      }
+                      if (_currentPage == 1) {
+                        if (auth.email == "" || auth.phoneNumber == "") {
+                          Fluttertoast.showToast(
+                            msg: "Please fill the above details fully.",
+                          );
+                          return;
+                        }
+                      }
+                      _controller.nextPage(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.decelerate,
+                      );
+                    } else {
+                      if (auth.idDoc == "" ||
+                          auth.idNumber == "" ||
+                          auth.panNumber == "" ||
+                          auth.idDocFile == File("") ||
+                          auth.panDocFile == File("")) {
+                        Fluttertoast.showToast(
+                          msg: "Please fill the above details fully.",
+                        );
+                        return;
+                      }
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      final SharedPreferences prefs =
+                          await SharedPreferences.getInstance();
+                      prefs.setBool("kyc_done", true);
+                      await ContractService.initUser(auth: auth);
+                      setState(() {
+                        _isLoading = false;
+                      });
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ProfileCompleteScreen(),
+                        ),
+                        (route) => false,
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: gold,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16,
+                      horizontal: 32,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        _currentPage == 2 ? "Submit" : "Next",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: _currentPage == 2
+                              ? FontWeight.w600
+                              : FontWeight.w400,
+                        ),
+                      ),
+                      if (_currentPage < 2)
+                        const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.black,
+                        ),
+                      if (_currentPage == 2 && _isLoading)
+                        const Padding(
+                          padding: EdgeInsets.only(left: 8),
+                          child: CircularProgressIndicator(
+                            color: Colors.black,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
