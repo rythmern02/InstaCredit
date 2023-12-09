0d81e77a455f95a37a8463db1bbdff7dfe4efbeb
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2023-12-09 22:12:47 +0530

    updating profile completed screen

--- Flutter_InstaCredit/lib/screens/profile_complete.dart
+++ Flutter_InstaCredit/lib/screens/profile_complete.dart
@@ -1,1 +1,116 @@
+import 'package:bnpl_flutter/constants.dart';
+import 'package:bnpl_flutter/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+class ProfileCompleteScreen extends StatefulWidget {
+  const ProfileCompleteScreen({super.key});
+
+  @override
+  State<ProfileCompleteScreen> createState() => _ProfileCompleteScreenState();
+}
+
+class _ProfileCompleteScreenState extends State<ProfileCompleteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SvgPicture.asset(
+            'assets/images/background.svg',
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 18.0),
+                  child: Text(
+                    "Congratulations",
+                    style: TextStyle(
+                      color: gold,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const Text(
+                  "You",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                const Text(
+                  "Have A",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                const Text(
+                  "Credit Limit",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                const Text(
+                  "of",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                const Text(
+                  "\$250",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 64,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeScreen(),
+                      ),
+                      (route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: gold,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16,
+                      horizontal: 32,
+                    ),
+                  ),
+                  child: const Text(
+                    "Spend Now",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
