4d48fe35a6aea723177affefd4a1fefaa48d3963
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2023-12-10 00:29:46 +0530

    updating the transaction history page

--- Flutter_InstaCredit/lib/screens/transaction_history.dart
+++ Flutter_InstaCredit/lib/screens/transaction_history.dart
@@ -1,1 +1,106 @@
+import 'package:bnpl_flutter/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+class TransactionHistoryScreen extends StatefulWidget {
+  const TransactionHistoryScreen({super.key});
+
+  @override
+  State<TransactionHistoryScreen> createState() =>
+      _TransactionHistoryScreenState();
+}
+
+class _TransactionHistoryScreenState extends State<TransactionHistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SvgPicture.asset(
+            'assets/images/background.svg',
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.only(left: 0),
+                  ),
+                  child: const Icon(
+                    Icons.keyboard_arrow_left_rounded,
+                    color: Color(0xFFD8D8D8),
+                    size: 60,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "HISTORY",
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: gold,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 24),
+                        child: Text(
+                          "Transactions",
+                          style: TextStyle(
+                            fontSize: 32,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                      ...List.generate(
+                        5,
+                        (index) => const Padding(
+                          padding: EdgeInsets.only(bottom: 16),
+                          child: Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  "Lorem ipsum dolor sit amet, consectetuer......",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "-\$100",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: gold,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
