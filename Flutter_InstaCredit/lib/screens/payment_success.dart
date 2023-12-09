0dcefc38a13d11e820a6ab8115a8fd7c229baa22
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2023-12-09 21:23:09 +0530

    rendered payment success screen

--- Flutter_InstaCredit/lib/screens/payment_success.dart
+++ Flutter_InstaCredit/lib/screens/payment_success.dart
@@ -1,1 +1,122 @@
+import 'package:bnpl_flutter/constants.dart';
+import 'package:bnpl_flutter/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class PaymentSuccess extends StatefulWidget {
+  final num amount;
+  const PaymentSuccess({
+    super.key,
+    required this.amount,
+  });
+
+  @override
+  State<PaymentSuccess> createState() => _PaymentSuccessState();
+}
+
+class _PaymentSuccessState extends State<PaymentSuccess> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SvgPicture.asset(
+            'assets/images/background.svg',
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(24.0),
+                  margin: const EdgeInsets.only(bottom: 56),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10000),
+                    color: Colors.transparent,
+                    border: Border.all(
+                      color: gold,
+                      width: 5,
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.check_rounded,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ),
+                Text(
+                  "Your",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                Text(
+                  "Payment Of",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                Text(
+                  "\$${widget.amount} Is",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                Text(
+                  "Successful!!!",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 24),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomeScreen(),
+                        ),
+                        (route) => false,
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: gold,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 32,
+                      ),
+                    ),
+                    child: Text(
+                      "Back to Home",
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
