d6f022ad087112e20185a24d9cd0342a663c9dde
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2023-12-10 07:41:51 +0530

    updating config , constants , and main file for the project

--- Flutter_InstaCredit/lib/constants.dart
+++ Flutter_InstaCredit/lib/constants.dart
@@ -594,3 +594,205 @@ String abi() => jsonEncode(
         }
       ],
     );
+
+String liquidityAbi() => jsonEncode(
+      [
+        {
+          "inputs": [
+            {"internalType": "address", "name": "_usdcToken", "type": "address"}
+          ],
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "inputs": [
+            {"internalType": "address", "name": "target", "type": "address"}
+          ],
+          "name": "AddressEmptyCode",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {"internalType": "address", "name": "account", "type": "address"}
+          ],
+          "name": "AddressInsufficientBalance",
+          "type": "error"
+        },
+        {"inputs": [], "name": "EnforcedPause", "type": "error"},
+        {"inputs": [], "name": "ExpectedPause", "type": "error"},
+        {"inputs": [], "name": "FailedInnerCall", "type": "error"},
+        {
+          "inputs": [
+            {"internalType": "address", "name": "token", "type": "address"}
+          ],
+          "name": "SafeERC20FailedOperation",
+          "type": "error"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "user",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "amount",
+              "type": "uint256"
+            }
+          ],
+          "name": "EarningsWithdrawn",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            }
+          ],
+          "name": "Paused",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "user",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "amount",
+              "type": "uint256"
+            }
+          ],
+          "name": "Staked",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            }
+          ],
+          "name": "Unpaused",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "user",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "amount",
+              "type": "uint256"
+            }
+          ],
+          "name": "Unstaked",
+          "type": "event"
+        },
+        {
+          "inputs": [
+            {"internalType": "address", "name": "_user", "type": "address"}
+          ],
+          "name": "getUserInfo",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "stakedAmount",
+              "type": "uint256"
+            },
+            {"internalType": "uint256", "name": "tvl", "type": "uint256"},
+            {
+              "internalType": "uint256",
+              "name": "earningYield",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "lockupDuration",
+          "outputs": [
+            {"internalType": "uint256", "name": "", "type": "uint256"}
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "paused",
+          "outputs": [
+            {"internalType": "bool", "name": "", "type": "bool"}
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {"internalType": "uint256", "name": "_amount", "type": "uint256"}
+          ],
+          "name": "stake",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {"internalType": "address", "name": "", "type": "address"}
+          ],
+          "name": "stakes",
+          "outputs": [
+            {"internalType": "uint256", "name": "amount", "type": "uint256"},
+            {"internalType": "uint256", "name": "timestamp", "type": "uint256"}
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "unstake",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "usdcToken",
+          "outputs": [
+            {"internalType": "contract IERC20", "name": "", "type": "address"}
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "withdrawEarnings",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ],
+    );
